49a080fc7de4e6ec264ae6864387dfe0f6e8f61f
MrShivam7 <122256610+MrShivam7@users.noreply.github.com>
2024-01-08 21:39:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt1 =
   "Aur tum jo sab mujhe kehte rehte ho ki usse bhul jao < Aur tum jo sab mujhe kehte rehte ho ki usse bhul jao < Tum hi btao kisiko bhulana itna asan hota hai kya < jis ko dil me basaya ho fhir, dil se nikalna itna asan hota hai kya < asan hai kya bhulana us shaks se ki hui baate < asan hai kya bhulana uske saath bitai hui vo raate < itna asan hota hai kya kisi ko paraya kr dena jis me tumhari duniya basti ho < usse apni duniya se alwida kr dena < are vo dil kashi nhi, ishq hai mera < are vo dil kashi nhi, ishq hai mera < mai chaha kr bhi usse bhula nhi paata < uske liye tadapte huye bhi us se baat nhi krta < ab dekho yaaro is se zyada sabar mai dikha nhi pata < vo meri hai jinn khawabo me, un khawabo ko mai mita nhi pata < mai har roz sochta hu, ki ab usse yaad na kru < par ab uski yaad ke bina mujhe saans nhi aata!! ";
-var speed = 55;
+var speed = 65;
 typeWriter();
 function typeWriter() {
   if (i < txt1.length) {
